62c52ac4eb44c1693b942bfbb0c53d1d675587a3
Raisa Grishina <bbbaaa987@mail.ru>
2025-01-19 01:09:52 +0300

    new rep made

---
+++ lesson_2/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+{
+  const item = 'smart проектор philips';
+  const count = 15;
+  const category = 'видео техника';
+  const price = 1200;
+
+  console.log(item);
+  console.log(`общая сумма товара: ${count * price}`);
+}
+
+{
+  const item = 'смартфон iPhone 15';
+  const count = 10;
+  const category = 'смартфоны';
+  const price = 78000;
+
+  console.log(item);
+  console.log(`общая сумма товара: ${count * price}`);
+}
